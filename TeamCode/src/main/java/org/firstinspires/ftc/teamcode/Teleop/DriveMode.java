2a7427cdd34b5115a4d7ff6ef0a5d241c8d86a39
Alex <alexiscopado22@gmail.com>
2024-09-26 17:42:08 -0700

    Renamed motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DriveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DriveMode.java
@@ -2,16 +2,18 @@ package org.firstinspires.ftc.teamcode.Teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Drive Mode", group="Drive Mode")
 public class DriveMode extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
+    private DcMotor FrontLeft = null;
+    private DcMotor FrontRight = null;
+    private DcMotor BackLeft = null;
+    private DcMotor BackRight = null;
+
 
     @Override
     public void runOpMode() {
@@ -36,15 +38,11 @@ public class DriveMode extends LinearOpMode {
         double lateral =  gamepad1.left_stick_x;
         double yaw     =  gamepad1.right_stick_x;
 
-        // Combine the joystick requests for each axis-motion to determine each wheel's power.
-        // Set up a variable for each drive wheel to save the power level for telemetry.
         double leftFrontPower  = axial + lateral + yaw;
         double rightFrontPower = axial - lateral - yaw;
         double leftBackPower   = axial - lateral + yaw;
         double rightBackPower  = axial + lateral - yaw;
 
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
         max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
         max = Math.max(max, Math.abs(leftBackPower));
         max = Math.max(max, Math.abs(rightBackPower));
@@ -56,10 +54,10 @@ public class DriveMode extends LinearOpMode {
             rightBackPower  /= max;
         }
 
-        leftFrontDrive.setPower(leftFrontPower);
-        rightFrontDrive.setPower(rightFrontPower);
-        leftBackDrive.setPower(leftBackPower);
-        rightBackDrive.setPower(rightBackPower);
+        FrontLeft.setPower(leftFrontPower);
+        FrontRight.setPower(rightFrontPower);
+        BackLeft.setPower(leftBackPower);
+        BackRight.setPower(rightBackPower);
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
@@ -69,15 +67,15 @@ public class DriveMode extends LinearOpMode {
 
     //This method is used to setup the hardware motors and sensors need to be setup here.
     private void SetupHardware() {
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        FrontLeft  = hardwareMap.get(DcMotor.class, "frontleft");
+        FrontRight = hardwareMap.get(DcMotor.class, "frontright");
+        BackLeft  = hardwareMap.get(DcMotor.class, "backleft");
+        BackRight = hardwareMap.get(DcMotor.class, "backright");
 
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
     }
 }
 
