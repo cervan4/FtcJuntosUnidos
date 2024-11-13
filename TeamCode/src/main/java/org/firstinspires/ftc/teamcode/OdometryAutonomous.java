b6ccc818eca9dfbade2f21bec7ec2172b17aad76
Alex <alexiscopado22@gmail.com>
2024-11-12 18:02:28 -0800

    Added missing code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryAutonomous.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
+@Autonomous(name="Odometry", group="Robot")
 public class OdometryAutonomous extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor FrontLeft = null;
