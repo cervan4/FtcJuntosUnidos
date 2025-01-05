6316c55faa8ef310eb229658a86e526d8c5a1552
Alex <alexiscopado22@gmail.com>
2025-01-04 17:55:01 -0800

    normal drive simple

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DriveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DriveMode.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 
 @TeleOp(name="Drive Mode", group="Drive Mode")
 public class DriveMode extends LinearOpMode {
@@ -13,69 +19,121 @@ public class DriveMode extends LinearOpMode {
     private DcMotor FrontRight = null;
     private DcMotor BackLeft = null;
     private DcMotor BackRight = null;
-
+    private DcMotor VerticalLiftMotor = null;
+    private DcMotor IntakeVerticalMotor = null;
+    private DcMotor IntakeHorizontalMotor = null;
+    private Servo ClawServo = null;
+    private CRServo IntakeServo = null;
+    static final double MAX_POS = 1.0;
+    static final double MIN_POS = 0.5;
 
     @Override
     public void runOpMode() {
-        SetupHardware();
+
+        FrontLeft = hardwareMap.get(DcMotor.class, "frontleft");
+        FrontRight = hardwareMap.get(DcMotor.class, "frontright");
+        BackLeft = hardwareMap.get(DcMotor.class, "backleft");
+        BackRight = hardwareMap.get(DcMotor.class, "backright");
+        ClawServo = hardwareMap.get(Servo.class, "Clawservo");
+        IntakeServo = hardwareMap.get(CRServo.class, "intakeservo");
+        VerticalLiftMotor = hardwareMap.get(DcMotor.class, "verticalLift");
+        IntakeVerticalMotor = hardwareMap.get(DcMotor.class, "intakeVertical");
+        IntakeHorizontalMotor = hardwareMap.get(DcMotor.class, "intakeHorizontal");
+
+
+        VerticalLiftMotor.setDirection(DcMotor.Direction.REVERSE);
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);//r
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);//r
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);//f
+        BackRight.setDirection(DcMotor.Direction.FORWARD);//f
+
+        FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //SetupHardware();
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
         runtime.reset();
 
+
         while (opModeIsActive()) {
-            DriveFunctionality();
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial = -gamepad1.left_stick_x;  // Note: pushing stick forward gives negative value
+            double lateral = gamepad1.left_stick_y;
+            double yaw = -gamepad1.right_stick_x;
+
+            boolean open = gamepad1.right_bumper;
+            boolean close = gamepad1.left_bumper;
+
+            if (open) {
+                ClawServo.setPosition(MAX_POS);
+            } else if (close) {
+                ClawServo.setPosition(MIN_POS);
+            }
+
+            if (gamepad1.x) {
+                IntakeServo.setPower(-1);
+            } else if (gamepad1.b) {
+                IntakeServo.setPower(1);
+            } else {
+                IntakeServo.setPower(0);
+            }
+
+            if (gamepad1.dpad_up) {
+                VerticalLiftMotor.setPower(1);
+            } else if (gamepad1.dpad_down) {
+                VerticalLiftMotor.setPower(-1);
+            } else if (gamepad1.y) {
+                VerticalLiftMotor.setPower(0.1);
+            } else {
+                VerticalLiftMotor.setPower(0);
+            }
+
+            if (gamepad1.dpad_right) {
+                IntakeHorizontalMotor.setPower(-1);
+            } else if (gamepad1.dpad_left) {
+                IntakeHorizontalMotor.setPower(1);
+            } else {
+                IntakeHorizontalMotor.setPower(0);
+            }
+
+
+            IntakeVerticalMotor.setPower(-gamepad1.right_trigger);
+            IntakeVerticalMotor.setPower(gamepad1.left_trigger);
+
+
+            double leftFrontPower = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
+
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower /= max;
+                rightFrontPower /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
+            }
+            FrontLeft.setPower(leftFrontPower);
+            FrontRight.setPower(rightFrontPower);
+            BackLeft.setPower(leftBackPower);
+            BackRight.setPower(rightBackPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.update();
         }
-    }
 
-    //This function controls basic function such as moving forward, Backward, Left and Right.
-    private void DriveFunctionality() {
-        double max;
-
-        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-        double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-        double lateral =  gamepad1.left_stick_x;
-        double yaw     =  gamepad1.right_stick_x;
-
-        double leftFrontPower  = axial + lateral + yaw;
-        double rightFrontPower = axial - lateral - yaw;
-        double leftBackPower   = axial - lateral + yaw;
-        double rightBackPower  = axial + lateral - yaw;
-
-        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
-
-        if (max > 1.0) {
-            leftFrontPower  /= max;
-            rightFrontPower /= max;
-            leftBackPower   /= max;
-            rightBackPower  /= max;
-        }
-
-        FrontLeft.setPower(leftFrontPower);
-        FrontRight.setPower(rightFrontPower);
-        BackLeft.setPower(leftBackPower);
-        BackRight.setPower(rightBackPower);
-
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-        telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-        telemetry.update();
-    }
-
-    //This method is used to setup the hardware motors and sensors need to be setup here.
-    private void SetupHardware() {
-        FrontLeft  = hardwareMap.get(DcMotor.class, "frontleft");
-        FrontRight = hardwareMap.get(DcMotor.class, "frontright");
-        BackLeft  = hardwareMap.get(DcMotor.class, "backleft");
-        BackRight = hardwareMap.get(DcMotor.class, "backright");
-
-        FrontLeft.setDirection(DcMotor.Direction.FORWARD);
-        BackLeft.setDirection(DcMotor.Direction.FORWARD);
-        FrontRight.setDirection(DcMotor.Direction.REVERSE);
-        BackRight.setDirection(DcMotor.Direction.REVERSE);
     }
 }
 
